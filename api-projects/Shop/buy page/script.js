9e987085329ef628883cc09b517fec3b934ebc5a
Alan <i39u65f51q@gmail.com>
2022-04-24 22:24:15 +0800

    upgrade Carousel

--- api-projects/Shop/buy page/script.js
+++ api-projects/Shop/buy page/script.js
@@ -1,10 +1,9 @@
 //Carousel
 const carouselImgs = document.querySelectorAll('.carousel img');
 const controlBtns = document.querySelectorAll('.carousel i.bx');
-const manualBtns = Array.from(document.querySelectorAll('.carousel-maunal li'));
+const manualBtns = document.querySelectorAll('.carousel-maunal li');
 let slide = 0;
-function slideNext() {
-  slide >= carouselImgs.length - 1 ? (slide = 0) : slide++;
+function carouselTransform() {
   carouselImgs.forEach(img => {
     img.style.transform = `translateX(${slide * -100}%)`;
   });
@@ -13,16 +12,13 @@ function slideNext() {
     .classList.remove('active');
   manualBtns[slide].classList.add('active');
 }
+function slideNext() {
+  slide >= carouselImgs.length - 1 ? (slide = 0) : slide++;
+  carouselTransform();
+}
 function slidePrev() {
   slide <= 0 ? (slide = carouselImgs.length - 1) : slide--;
-  carouselImgs.forEach(img => {
-    img.style.transform = `translateX(${slide * -100}%)`;
-  });
-  document
-    .querySelector('.carousel-maunal li.active')
-    .classList.remove('active');
-  manualBtns[slide].classList.add('active');
-  console.log(slide);
+  carouselTransform();
 }
 setInterval(slideNext, 6000);
 controlBtns.forEach(btn => {
@@ -39,10 +35,15 @@ manualBtns.forEach((btn, i) => {
   btn.addEventListener('click', () => {
     document.querySelector('.carousel li.active').classList.remove('active');
     btn.classList.add('active');
-    slide = i;
     carouselImgs.forEach(img => {
       img.style.transform = `translateX(${i * -100}%)`;
     });
     // console.log(i);
   });
+});
+//Carts Page
+const toggleCartBtn = document.querySelector('button.cart');
+const cartPage = document.querySelector('.cart-page');
+toggleCartBtn.addEventListener('click', () => {
+  cartPage.classList.toggle('show');
 });
