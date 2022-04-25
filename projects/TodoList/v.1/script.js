f7f825ea6d5cf87ffb4eb2214bd2054c27dea6fc
Alan <i39u65f51q@gmail.com>
2022-04-25 12:16:22 +0800

    update Todo v.1

--- projects/TodoList/v.1/script.js
+++ projects/TodoList/v.1/script.js
@@ -1,36 +1,43 @@
-const input = document.querySelector('input[type="text"]');
+const input = document.querySelector('header input');
 const addBtn = document.querySelector('.added-btn');
-const listContent = document.querySelector('.list');
-
 const list = [];
-
 function addItem() {
-  let li = '';
-  li += `<li>
-      <p>${input.value}</p>
-      <button class="checked-btn"><i class="bx bx-check"></i></button>
-      <button class="deleted-btn"}><i class="bx bxs-trash"></i></button>
-      </li>`;
+  const ul = document.querySelector('ul');
+  let li = ` <li>
+  <span>${input.value}</span>
+  <button class="checked-btn list-btn">
+    <i class="bx bx-check"></i>
+  </button>
+  <button class="deleted-btn list-btn">
+    <i class="bx bxs-trash"></i>
+  </button>
+</li>`;
   list.push(li);
-  listContent.innerHTML = list.join('');
+  ul.innerHTML = list.join('');
   input.value = '';
-}
-function buttonHandler(e) {
-  const btn = e.target;
-  if (btn.classList[0] == 'checked-btn') {
-    btn.parentNode.classList.toggle('finished');
-  } else if (btn.classList[0] == 'deleted-btn') {
-    btn.parentNode.classList.add('removed');
 
-    btn.parentNode.addEventListener('transitionend', () => {
-      btn.parentNode.remove();
-      //還沒找到方法取index
-      // deletedItem(index);
+  const btns = document.querySelectorAll('.list-btn');
+  btns.forEach((btn, i) => {
+    btn.addEventListener('click', e => {
+      if (e.target.classList.contains('checked-btn')) {
+        const span = e.target.parentNode.firstElementChild;
+        span.classList.toggle('finished');
+      }
+      if (e.target.classList.contains('deleted-btn')) {
+        const li = e.target.parentNode;
+        li.classList.add('removed');
+        li.addEventListener('transitionend', () => {
+          deleteItem();
+          li.remove();
+        });
+      }
     });
-  }
+  });
 }
-function deletedItem(index) {
-  list.splice(index, 1);
+function deleteItem(e) {
+  const deleteBtns = document.querySelectorAll('.deleted-btn');
+  deleteBtns.forEach((btn, i) => {
+    list.splice(i, 1);
+  });
 }
 addBtn.addEventListener('click', addItem);
-listContent.addEventListener('click', buttonHandler);
